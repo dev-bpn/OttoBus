d61028775324a820260c04eb38ab189ac665a485
dev-bpn <jazzbpn@gmail.com>
2015-12-15 17:17:10 +0545

    Volley Job

--- app/src/main/java/mobotech/ottobus/MainActivity.java
+++ app/src/main/java/mobotech/ottobus/MainActivity.java
@@ -1,16 +1,55 @@
 package mobotech.ottobus;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
+    String url = "http://api.androidhive.info/volley/person_object.json";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
+        simpleVolleyRequest();
+
+    }
+
+    public void simpleVolleyRequest(){
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET, url, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Toast.makeText(getApplicationContext() , response.toString(), Toast.LENGTH_SHORT).show();
+                Log.i("TAG" , "Response: "+response.toString());
+                textView.setText(response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext() , "Error: "+error.toString(), Toast.LENGTH_SHORT).show();
+                Log.i("TAG", "Error: " + error.toString());
+                textView.setText(error.toString());
+            }
+        });
+        requestQueue.add(jsonObjReq);
+
     }
 
     @Override
